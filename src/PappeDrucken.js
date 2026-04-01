d6eaef6b367e27ff17cac7c4b9d17b364bc2566c
OKh <hudoshin7605@gmail.com>
2026-04-01 09:56:13 +0200

    PappelDrucken.js added,

---
+++ src/PappeDrucken.js
@@ -0,0 +1,14 @@
+let pappe = [3, 4, 0.5, 5];
+let notaus = isNotaus();
+function pappeDrucken(b, l, d){}
+
+function isNotaus() {
+    return undefined;
+}
+
+for (let i = 0; i < pappe[3]; i++) {
+    if (notaus === false) {
+        pappeDrucken(pappe[0], pappe[1], pappe[2]);
+        notaus = isNotaus();
+    }
+}
